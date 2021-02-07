a9c29ee4d413e25db32a02c10d8a25da033b63da
peter <peterkaboto9@gmail.com>
2021-02-07 12:07:05 +0200

    added create patient validation, made create patient live

--- lib/screens/CreatePatient.dart
+++ lib/screens/CreatePatient.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:loading_dialog/loading_dialog.dart';
+import 'package:mediquest_mobile/managers/QuestionnaireManager.dart';
+import 'package:mediquest_mobile/models/Patient.dart';
+import 'package:mediquest_mobile/utils/DummyData.dart';
 
 // so we're create a normal startup of a flutter application here
 // so in our MyHomePage is where we're going to be working
@@ -14,8 +18,12 @@ class CreatePatient extends StatefulWidget {
 
 class _CreatePatientState extends State<CreatePatient> {
   // So all this four controller's is for the textfield we're creating
-  var _formKey = GlobalKey<ScaffoldState>(); // a scaffold Globalkey
+  final _formKey = new GlobalKey<FormState>();
   bool hidePass = true;
+  String _initials;
+  DateTime dateOfBirth = DateTime(1900, 12, 12);
+  String _sex;
+  int age;
 
   // this is a boolean called hidePass we're going to use it for show and hide password
   @override
@@ -30,117 +38,147 @@ class _CreatePatientState extends State<CreatePatient> {
           body: Card(
             elevation: 2,
             shadowColor: Colors.grey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Center(
-                  child: Text(
-                    'Patient Registration',
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.w300,
+            child: SingleChildScrollView(
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Center(
+                      child: Text(
+                        'Patient Registration',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.w300,
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(20),
-                  child: Column(
-                    children: <Widget>[
-                      // textfield for the name session and so on
-                      SizedBox(height: 10),
-                      TextFormField(
-                        decoration: InputDecoration(
-                          labelText: 'initials',
-                          icon: Icon(Icons.mail),
-                        ),
-                      ),
-                      SizedBox(height: 10),
-                      Container(
-                        child: DropdownButtonFormField(
-                          items: [
-                            DropdownMenuItem(child: Text("Male")),
-                            DropdownMenuItem(child: Text("Female"))
-                          ],
-                          decoration: InputDecoration(
-                              icon: Icon(Icons.workspaces_filled)),
-                          onChanged: (value) {},
-                          hint: Text("Sex"),
-                        ),
-                      ),
-                      SizedBox(height: 10),
-                      TextFormField(
-                        keyboardType: TextInputType.number,
-                        decoration: InputDecoration(
-                          labelText: 'Age',
-                          icon: Icon(Icons.accessibility),
-                        ),
-                      ),
-                      SizedBox(height: 10),
-
-                      /* Column(
-                         children: [
-                           Text("Date of Birth"),
-                           SizedBox(height: 15),
-                           SizedBox(
-                             height: 150,
-
-                             child: CupertinoDatePicker(onDateTimeChanged: (newDate){
-
-                             },
-                               mode: CupertinoDatePickerMode.date,
-
-                             ),
-                           ),
-                         ],
-                       ),*/
-                      FormBuilderDateTimePicker(
-                        name: "here",
-                        initialDatePickerMode: null,
-                        initialEntryMode: DatePickerEntryMode.calendar,
-                        decoration: InputDecoration(
-                          icon: Icon(Icons.ac_unit_sharp),
-                        ),
-                      ),
-                      SizedBox(height: 10),
-                      Row(
+                    Padding(
+                      padding: EdgeInsets.all(20),
+                      child: Column(
                         children: <Widget>[
-                          Expanded(
-                            child: MaterialButton(
-                              color: Theme.of(context).accentColor,
-                              child: const Text(
-                                'Submit',
-                                style: TextStyle(color: Colors.white),
-                              ),
-                              onPressed: () {
-                                /* if (_formKey.currentState.saveAndValidate()) {
-                          print(_formKey.currentState.value);
-                        } else {
-                          print(_formKey.currentState.value);
-                          print('validation failed');
-                        }*/
+                          // textfield for the name session and so on
+                          SizedBox(height: 10),
+                          TextFormField(
+                            decoration: InputDecoration(
+                              labelText: 'initials',
+                              icon: Icon(Icons.mail),
+                            ),
+                            validator: (value) => value.isEmpty
+                                ? 'Initials can\'t be empty'
+                                : null,
+                            onSaved: (value) => _initials = value.trim(),
+                          ),
+                          SizedBox(height: 10),
+                          //sex
+                          Container(
+                            child: FormBuilderRadioGroup(
+                              options: [
+                                FormBuilderFieldOption(value: "Male"),
+                                FormBuilderFieldOption(value: "Female"),
+                              ],
+                              initialValue: "",
+                              decoration: InputDecoration(
+                                  icon: Icon(Icons.workspaces_filled)),
+                              onSaved: (value) {
+                                _sex = value;
+                              },
+                              validator: (value) {
+                                return value.toString().isEmpty
+                                    ? "Sex can\'t be null"
+                                    : null;
                               },
                             ),
                           ),
-                          const SizedBox(width: 20),
-                          Expanded(
-                            child: OutlineButton(
-                              color: Theme.of(context).accentColor,
-                              child: Text(
-                                'Reset',
-                                style: TextStyle(
-                                    color: Theme.of(context).accentColor),
-                              ),
-                              onPressed: () {
-                                /* _formKey.currentState.reset();*/
-                              },
+                          SizedBox(height: 10),
+                          //age
+                          TextFormField(
+                            keyboardType: TextInputType.number,
+                            decoration: InputDecoration(
+                              labelText: 'Age',
+                              icon: Icon(Icons.accessibility),
                             ),
+                            onSaved: (value) => _sex = value,
+                            validator: (value) =>
+                                value.isEmpty ? 'Age can\'t be empty' : null,
+                          ),
+                          SizedBox(height: 10),
+
+                          //DOB
+                          FormBuilderDateTimePicker(
+                            name: "here",
+                            onSaved: (value) => dateOfBirth = value,
+                            initialDatePickerMode: null,
+                            initialEntryMode: DatePickerEntryMode.calendar,
+                            decoration: InputDecoration(
+                              icon: Icon(Icons.ac_unit_sharp),
+                            ),
+                            validator: (value) => value == null
+                                ? 'Date of birth can\'t be empty'
+                                : null,
+                          ),
+                          SizedBox(height: 10),
+                          Row(
+                            children: <Widget>[
+                              Expanded(
+                                child: MaterialButton(
+                                  color: Theme.of(context).accentColor,
+                                  child: const Text(
+                                    'Submit',
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                  onPressed: () async {
+                                    if (_formKey.currentState.validate()) {
+                                      _formKey.currentState.save();
+                                      print(_formKey.currentState);
+
+                                      LoadingDialog loading = LoadingDialog(
+                                          buildContext: context,
+                                          loadingMessage: "Creating ");
+                                      loading.show();
+                                      Patient patient =
+                                          await QuestionnaireManager()
+                                              .addPatient(Patient(
+                                                  1,
+                                                  DummyData.questionaire,
+                                                  "OK",
+                                                  DateTime.now(),
+                                                  age,
+                                                  _sex,
+                                                  DummyData.institution,
+                                                  DateTime.now(),
+                                                  DateTime.now()));
+                                      print(patient.toJson());
+                                      loading.hide();
+                                    } else {
+                                      print(_formKey.currentState);
+                                      print('validation failed');
+                                    }
+                                  },
+                                ),
+                              ),
+                              const SizedBox(width: 20),
+                              Expanded(
+                                child: OutlineButton(
+                                  color: Theme.of(context).accentColor,
+                                  child: Text(
+                                    'Reset',
+                                    style: TextStyle(
+                                        color: Theme.of(context).accentColor),
+                                  ),
+                                  onPressed: () {
+                                    _formKey.currentState.reset();
+                                  },
+                                ),
+                              ),
+                            ],
                           ),
                         ],
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
           ),
         ),
@@ -154,7 +192,7 @@ class _CreatePatientState extends State<CreatePatient> {
     });
   }
 
-  void _showInSnackBar({String message}) {
+/*  void _showInSnackBar({String message}) {
     _formKey.currentState.showSnackBar(
       SnackBar(
         content: GestureDetector(
@@ -169,45 +207,7 @@ class _CreatePatientState extends State<CreatePatient> {
         backgroundColor: Colors.black,
       ),
     );
-  }
-
-  formValidate({String name, email, phn, pass, cpass}) {
-    if (name.toString().isEmpty) {
-      _showInSnackBar(message: 'Full name required');
-    } else if (!email.toString().contains('@')) {
-      _showInSnackBar(message: 'Invalid email address');
-    } else if (phn.toString().isEmpty || phn.length != 11) {
-      _showInSnackBar(message: 'Invalid phone number');
-    } else if (pass.toString().isEmpty || pass.length != 8) {
-      _showInSnackBar(message: '8 character required for password');
-    } else if (cpass.toString() != pass.toString()) {
-      _showInSnackBar(message: 'Password does not match');
-    } else {
-      openDia(name: name);
-    }
-  }
-
-  openDia({String name}) {
-    showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            content: Text('$name is now a verified account'),
-            title: Text('Registration Successful'),
-            actions: <Widget>[
-              MaterialButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: Text(
-                  'Verified',
-                  style: TextStyle(color: Colors.blue),
-                ),
-              )
-            ],
-          );
-        });
-  }
+  }*/
 
   @override
   void initState() {

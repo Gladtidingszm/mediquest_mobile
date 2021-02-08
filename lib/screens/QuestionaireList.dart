50e58ef4dd1052d88eed91ba908bef7ff71705f6
peter <peterkaboto9@gmail.com>
2021-02-08 13:18:41 +0200

    made questionnaire list live

--- lib/screens/QuestionaireList.dart
+++ lib/screens/QuestionaireList.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:mediquest_mobile/components/questionaire_list_item.dart';
+import 'package:mediquest_mobile/components/QuestionaireListItem.dart';
+import 'package:mediquest_mobile/managers/QuestionnaireManager.dart';
 import 'package:mediquest_mobile/models/Lesson.dart';
+import 'package:mediquest_mobile/models/Questionnaire.dart';
 
 class ListPage extends StatefulWidget {
   ListPage({Key key, this.title}) : super(key: key);
@@ -26,13 +28,49 @@ class _ListPageState extends State<ListPage> {
 
     final makeBody = Container(
       // decoration: BoxDecoration(color: Color.fromRGBO(58, 66, 86, 1.0)),
-      child: ListView.builder(
-        scrollDirection: Axis.vertical,
-        shrinkWrap: true,
-        itemCount: lessons.length,
-        itemBuilder: (BuildContext context, int index) {
-          return QuestionaireListItem(lessons[index]);
-        },
+      child: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.all(20),
+          child: FutureBuilder(
+            builder: (context, AsyncSnapshot snap) {
+              switch (snap.connectionState) {
+                case ConnectionState.none:
+                  return Center(child: Text("No internet,  Please try later"));
+                  break;
+                case ConnectionState.waiting:
+                  return Center(child: CircularProgressIndicator());
+                  break;
+                case ConnectionState.active:
+                  return Center(child: Text("active"));
+                  break;
+                case ConnectionState.done:
+                  if (snap.hasData && snap.data.length != 0) {
+                    return Column(
+                      children: [
+                        ListView.builder(
+                          scrollDirection: Axis.vertical,
+                          shrinkWrap: true,
+                          itemCount: snap.data.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            return QuestionaireListItem(snap.data[index]);
+                          },
+                        ),
+                      ],
+                    );
+                  } else {
+                    return Container(
+                      child: Center(
+                        child: Text("Oops,  No data,Check You Have Internet"),
+                      ),
+                    );
+                  }
+                  break;
+              }
+              return Container();
+            },
+            future: getQuestionnaires(),
+          ),
+        ),
       ),
     );
 
@@ -79,8 +117,22 @@ class _ListPageState extends State<ListPage> {
       backgroundColor: Colors.white,
       appBar: topAppBar,
       body: makeBody,
-     // bottomNavigationBar: makeBottom,
+      // bottomNavigationBar: makeBottom,
     );
+  }
+}
+
+Future<List<Questionnaire>> getQuestionnaires() async {
+  print("getting questionnaires");
+  List<Questionnaire> questionnaires = List();
+
+  questionnaires = await QuestionnaireManager().getStudentQuestionnaires();
+
+  if (questionnaires != null && questionnaires.isNotEmpty) {
+    print("on data return questionnaires  list not empty");
+    return questionnaires;
+  } else {
+    print(" on data return questionnaires   list empty ");
   }
 }
 
@@ -92,7 +144,7 @@ List getLessons() {
         indicatorValue: 0.33,
         price: 20,
         content:
-        "Start by taking a couple of minutes to read the info in this section. Launch your app and click on the Settings menu.  While on the settings page, click the Save button.  You should see a circular progress indicator display in the middle of the page and the user interface elements cannot be clicked due to the modal barrier that is constructed."),
+            "Start by taking a couple of minutes to read the info in this section. Launch your app and click on the Settings menu.  While on the settings page, click the Save button.  You should see a circular progress indicator display in the middle of the page and the user interface elements cannot be clicked due to the modal barrier that is constructed."),
     Lesson(
         title: "Commerce",
         level: "Progress",

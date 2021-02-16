d947b07414158f31e3d906c394f49b10803772ec
peter <peterkaboto9@gmail.com>
2021-02-16 13:02:48 +0200

    refactored assignment serialisation

--- lib/models/Assignment.dart
+++ lib/models/Assignment.dart
@@ -1,40 +1,57 @@
-import 'package:json_annotation/json_annotation.dart';
-import 'package:mediquest_mobile/models/Institution.dart';
-
-import 'Supervisor.dart';
-
-part 'Assignment.g.dart';
-
-@JsonSerializable(explicitToJson: true)
 class Assignment {
   int id;
-  @JsonKey(name: "assignment_number")
   String assignmentNumber;
   String description;
-  @JsonKey(name: "date_issued")
-  DateTime dateIssued;
-  @JsonKey(name: "due_date")
-  DateTime dueDate;
-  Supervisor staff;
-  Institution institution;
-  @JsonKey(name: "created_at")
-  DateTime createdAt;
-  @JsonKey(name: "updated_at")
-  DateTime updatedAt;
+  String dateIssued;
+  String dueDate;
+  int courseId;
+  int staffId;
+  int institutionId;
+  String createdAt;
+  String updatedAt;
 
   Assignment(
-      this.id,
+      {this.id,
       this.assignmentNumber,
       this.description,
       this.dateIssued,
       this.dueDate,
-      this.staff,
-      this.institution,
+      this.courseId,
+      this.staffId,
+      this.institutionId,
       this.createdAt,
-      this.updatedAt);
+      this.updatedAt});
 
-  factory Assignment.fromJson(Map<String, dynamic> json) =>
-      Assignment.fromJson(json);
+  Assignment.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    assignmentNumber = json['assignment_number'];
+    description = json['description'];
+    dateIssued = json['date_issued'];
+    dueDate = json['due_date'];
+    courseId = json['course_id'];
+    staffId = json['staff_id'];
+    institutionId = json['institution_id'];
+    createdAt = json['created_at'];
+    updatedAt = json['updated_at'];
+  }
 
-  Map<String, dynamic> toJson() => _$AssignmentToJson(this);
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['assignment_number'] = this.assignmentNumber;
+    data['description'] = this.description;
+    data['date_issued'] = this.dateIssued;
+    data['due_date'] = this.dueDate;
+    data['course_id'] = this.courseId;
+    data['staff_id'] = this.staffId;
+    data['institution_id'] = this.institutionId;
+    data['created_at'] = this.createdAt;
+    data['updated_at'] = this.updatedAt;
+    return data;
+  }
+
+  @override
+  String toString() {
+    return 'Assignment{id: $id, assignmentNumber: $assignmentNumber, description: $description, dateIssued: $dateIssued, dueDate: $dueDate, courseId: $courseId, staffId: $staffId, institutionId: $institutionId, createdAt: $createdAt, updatedAt: $updatedAt}';
+  }
 }
